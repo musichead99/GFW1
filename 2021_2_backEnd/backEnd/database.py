eb99b5e991ff1a7c7c402fcaf454541bf1901193
musichead99 <musichead99@naver.com>
2021-08-17 22:36:05 +0900

    주석 작성, 클라이언트로부터 전달된 인자들 무결성 체크 기능 추가

--- 2021_2_backEnd/backEnd/database.py
+++ 2021_2_backEnd/backEnd/database.py
@@ -2,7 +2,9 @@
 
 import pymysql
 
+# Mysql Connection을 위한 객체 정의
 class DBClass():
+    # mysql 계정 정보, 퀴리문 실행 후 리턴값은 dictionary형식으로 반환 
     def __init__(self):
         self.db = pymysql.connect(
             user        = 'root',
@@ -17,15 +19,18 @@ class DBClass():
     def execute(self, query, args):
         self.cursor.execute(query, args)
 
+    # 쿼리를 수행한 결과를 전부 반환
     def executeAll(self, query, args):
         self.cursor.execute(query, args)
         row = self.cursor.fetchall()
         return row
 
+    # 쿼리를 수행한 결과 중 최상위 row만 반환
     def executeOne(self, query, args):
         self.cursor.execute(query, args)
         row = self.cursor.fetchone()
         return row
 
+    # commit
     def commit(self):
         self.db.commit()
