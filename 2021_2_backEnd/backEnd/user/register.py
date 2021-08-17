ccbf0d1744e1917e7f55de4530d69be1d0b82839
musichead99 <musichead99@naver.com>
2021-08-17 17:22:55 +0900

    users table 구조 변경, register email 중복 방지 구현

--- 2021_2_backEnd/backEnd/user/register.py
+++ 2021_2_backEnd/backEnd/user/register.py
@@ -2,6 +2,7 @@
 from flask import request
 from flask_restx import Namespace, Resource
 from .. import database
+from pymysql import err
 
 Register = Namespace('Register')
 
@@ -9,12 +10,17 @@ Register = Namespace('Register')
 class register(Resource):
     def post(self):
         data = request.json
-        db_class = database.DBClass()
+        db = database.DBClass()
 
-        query = '''INSERT INTO users(email, password, name)
-            VALUES(%(email)s, %(password)s, %(name)s);
-            '''
-        db_class.execute(query, data)
-        db_class.commit()
-
-        return {"message":"Success"}
+        # 중복된 email 입력 시 예외 처리
+        try:
+            query = '''INSERT INTO users(email, password, name)
+                VALUES(%(email)s, %(password)s, %(name)s);
+                '''
+            db.execute(query, data)
+            message = "Register Success"
+        except err.IntegrityError:
+            message = "Register Failed(Email Duplicated)"
+        finally:
+            db.commit()
+        return {"message": message }
