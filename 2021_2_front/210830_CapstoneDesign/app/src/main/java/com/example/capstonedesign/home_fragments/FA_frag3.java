24a4243392bdc81d9cb5fbec2e5fb6d71693329d
avocadosmasher <rdd0426@gmail.com>
2021-10-20 19:54:55 +0900

    Youngwoo Laptop->Descktop
    노트북에서 잠시 작업하던 내용 올립니다.
    큰 변동사항은 없고 상태탭에서 걸음수 부분은 Mymenu에서 제거했습니다.
    (기본적으로 보여주는 활동 정보임으로 항상 보여주도록 하였습니다.)

--- 2021_2_front/210830_CapstoneDesign/app/src/main/java/com/example/capstonedesign/home_fragments/FA_frag3.java
+++ 2021_2_front/210830_CapstoneDesign/app/src/main/java/com/example/capstonedesign/home_fragments/FA_frag3.java
@@ -8,10 +8,18 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 import com.example.capstonedesign.R;
+import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataType;
 
 public class FA_frag3 extends Fragment {
 
     public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fa_frag3,container,false);
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fa_frag3,container,false);
+
+        FitnessOptions fitnessOptions = FitnessOptions.builder()
+                .addDataType(DataType.TYPE_DISTANCE_DELTA,FitnessOptions.ACCESS_READ)
+                .build();
+
+        return rootView;
     }
 }
