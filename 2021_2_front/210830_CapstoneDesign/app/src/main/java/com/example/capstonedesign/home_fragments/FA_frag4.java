16978aed8070b25efdafe2c21d02b65ccb978113
John <rdd0426@gmail.com>
2021-11-04 17:48:23 +0900

    위치탭 Ver 1.0
    위치탭에서 7일 30일 단위로 그래프 찍는 함수 Ver 1.0 만들어서 업데이트 합니다.

--- 2021_2_front/210830_CapstoneDesign/app/src/main/java/com/example/capstonedesign/home_fragments/FA_frag4.java
+++ 2021_2_front/210830_CapstoneDesign/app/src/main/java/com/example/capstonedesign/home_fragments/FA_frag4.java
@@ -2,7 +2,6 @@ package com.example.capstonedesign.home_fragments;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,41 +12,29 @@ import androidx.fragment.app.Fragment;
 
 import com.example.capstonedesign.R;
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.AxisBase;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.google.android.gms.fitness.FitnessOptions;
 import com.google.android.gms.fitness.data.DataType;
-import com.google.android.material.button.MaterialButton;
-import com.google.android.material.button.MaterialButtonToggleGroup;
 
-
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.concurrent.TimeUnit;
 
-public class FA_frag1 extends Fragment {
+public class FA_frag4 extends Fragment {
 
     public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fa_frag1,container,false);
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fa_frag4,container,false);
 
         Context appContext = rootView.getContext().getApplicationContext();
         Context curContext = rootView.getContext();
-        ToggleButton toggle_btn = rootView.findViewById(R.id.fa_frag1_toggleButton);
+        ToggleButton toggle_btn = rootView.findViewById(R.id.fa_frag4_toggleButton);
         boolean tf_period = toggle_btn.isChecked();
         float[] result = new float[30];
-        LineChart lineChart = rootView.findViewById(R.id.fa_frag1_lineChart);
+        LineChart lineChart = rootView.findViewById(R.id.fa_frag4_lineChart);
 
         Arrays.fill(result,(float)0);
 
         /** Google fit 데이터 불러오기. **/
         FitnessOptions fitnessOptions =
                 FitnessOptions.builder()
-                        .addDataType(DataType.TYPE_STEP_COUNT_DELTA,FitnessOptions.ACCESS_READ)
+                        .addDataType(DataType.TYPE_MOVE_MINUTES,FitnessOptions.ACCESS_READ)
                         .build();
 
         MyGoogleFit myGoogleFit = MyGoogleFit.getInstance()
@@ -55,7 +42,7 @@ public class FA_frag1 extends Fragment {
                 .setAppContext(appContext);
 
 
-        int dataType = MyGoogleFit.TYPE_STEP;
+        int dataType = MyGoogleFit.TYPE_MOVE_MIN;
         myGoogleFit.subscription(dataType,curContext)
                 .getPeriodicData(dataType,curContext,tf_period,result,lineChart);
 
