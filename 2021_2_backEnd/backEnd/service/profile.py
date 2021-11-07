bc9ae1180b22f282316997462ebd7a881ae81dd7
musichead99 <musichead99@naver.com>
2021-11-07 20:53:37 +0900

    profile api 조금 수정

--- 2021_2_backEnd/backEnd/service/profile.py
+++ 2021_2_backEnd/backEnd/service/profile.py
@@ -45,7 +45,7 @@ class userProfile(Resource):
             return {"status" : "Failed", "message" : "Email not registered"}, 400
         else:
             if profileData['profilePhoto'] is None:
-                profileData['profilePhoto'] = config.baseUrl + '/service/images/default_profile.jpg'
+                profileData['profilePhoto'] = config.baseUrl + '/service/image/default_profile.jpg'
 
             if profileData['dateOfBirth'] is not None:
                 profileData['dateOfBirth'] = date.isoformat(profileData['dateOfBirth'])
