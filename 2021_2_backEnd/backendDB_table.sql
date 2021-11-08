d6569634cc5ddf0a7072722e37d3a84bfb65c9e0
ghwns82 <68892482+ghwns82@users.noreply.github.com>
2021-11-08 15:37:26 +0900

    Add files via upload

---
+++ 2021_2_backEnd/backendDB_table.sql
@@ -0,0 +1,59 @@
+use backend_test;
+
+-- Table structure for table `revoked_tokens`
+DROP TABLE IF EXISTS revoked_tokens;
+CREATE TABLE revoked_tokens (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  jti varchar(500) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- Table structure for table `users`
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  email varchar(200) not NULL,
+  password varchar(200) DEFAULT NULL,
+  name varchar(200) DEFAULT NULL,
+  fcmToken varchar(200) DEFAULT NULL,
+  profilePhoto varchar(200) DEFAULT NULL,
+  PRIMARY KEY (email)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- Table structure for table `request_friend`
+DROP TABLE IF EXISTS request_friend;
+CREATE TABLE request_friend (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  requester varchar(200) DEFAULT NULL,
+  acceptor varchar(200) DEFAULT NULL,
+  PRIMARY KEY (id),
+  foreign key(requester) REFERENCES users(email) on update cascade on delete cascade,
+  foreign key(acceptor) REFERENCES users(email) on update cascade on delete cascade
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Table structure for table `friends`
+DROP TABLE IF EXISTS friends;
+CREATE TABLE friends (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  user_email varchar(200) DEFAULT NULL,
+  user_friend_email varchar(200) DEFAULT NULL,
+  PRIMARY KEY (id),
+  foreign key(user_email) REFERENCES users(email) on update cascade on delete cascade,
+  foreign key(user_friend_email) REFERENCES users(email) on update cascade on delete cascade
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Table structure for table `health_data`
+DROP TABLE IF EXISTS health_data;
+CREATE TABLE health_data (
+  user_email varchar(200) not NULL,
+  Date date not null,
+  step_count int default 0,				# 단위: 걸음
+  carories int default 0,				# 단위: kcal
+  distance int default 0,				# 단위: m
+  time int default 0,					# 단위: 분  
+  
+  PRIMARY KEY (user_email, Date),
+  foreign key(user_email) REFERENCES users(email) on update cascade on delete cascade
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
