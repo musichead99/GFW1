0cffe340aceb80810d1cd6c8b5ecce5720fc9332
KIMLEENA <gmldk728@naver.com>
2021-11-18 18:43:29 +0900

    뷰 동적 할당 진행중

--- 2021_2_front/210830_CapstoneDesign/app/src/main/java/com/example/capstonedesign/home_fragments/FL.java
+++ 2021_2_front/210830_CapstoneDesign/app/src/main/java/com/example/capstonedesign/home_fragments/FL.java
@@ -5,12 +5,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.Image;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -24,7 +26,10 @@ import com.example.capstonedesign.retrofit.RankingResponse;
 import com.example.capstonedesign.retrofit.RetrofitClient;
 import com.example.capstonedesign.retrofit.initMyApi;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import retrofit2.Call;
@@ -37,6 +42,8 @@ public class FL extends Fragment { //친구수 받아오는것 구현 필요
     ImageView profile_myphoto;
     TextView profile_name, profile_walk;
     ArrayList<Ranking> ranking;
+    //LinearLayout tv;
+    LinearLayout parent_layout;
 
     public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fl,container,false);
@@ -55,6 +62,10 @@ public class FL extends Fragment { //친구수 받아오는것 구현 필요
         profile_myphoto = rootView.findViewById(R.id.profile_myphoto);
         profile_name = rootView.findViewById(R.id.profile_name);
         profile_walk = rootView.findViewById(R.id.profile_walk);
+        parent_layout = rootView.findViewById(R.id.parent_layout);
+
+        //tv = rootView.findViewById(R.id.tv);
+
         getProfile();
         getRanking();
         return rootView;
@@ -96,7 +107,6 @@ public class FL extends Fragment { //친구수 받아오는것 구현 필요
         initMyApi = RetrofitClient.getRetrofitInterface();
 
         ranking = new ArrayList<Ranking>();
-
         Call<RankingResponse> call = initMyApi.getRankingResponse("Bearer "+mytoken);
         call.enqueue(new Callback<RankingResponse>() {
             @Override
@@ -104,9 +114,20 @@ public class FL extends Fragment { //친구수 받아오는것 구현 필요
                 if(response.isSuccessful()) {
                     RankingResponse result = response.body();
                     String status = result.getStatus();
-
                     List<Ranking> rank = result.getRanking();
-                    Log.d("랭킹은","랭킹"+rank);
+                    for(int i = 0; i<result.getRanking().size(); i++) {
+                        //TextView textViewNew = new TextView(getContext());
+                        //textViewNew.setText(result.getRanking().get(i).getName());
+                        //tv.addView(textViewNew);
+                        LinearLayout layout = new LinearLayout(getContext());
+                        ImageView profile_phogo_align = new ImageView(getContext());
+                        Glide.with(getActivity()).load(result.getRanking().get(i).getProfilePhoto()).into(profile_phogo_align);
+                        ViewGroup.LayoutParams profile_photo_params = profile_phogo_align.getLayoutParams();
+                        profile_photo_params.width=60;
+                        profile_photo_params.height=60;
+                        layout.addView(profile_phogo_align);
+                        parent_layout.addView(layout);
+                    }
                 }
             }
 
@@ -115,5 +136,14 @@ public class FL extends Fragment { //친구수 받아오는것 구현 필요
 
             }
         });
+    }
+    private void createTextView() {
+        TextView textViewNew = new TextView(getContext());
+        textViewNew.setText("");
+        textViewNew.setTextSize(18);
+        textViewNew.setId(0);
+        //LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        //textViewNew.setLayoutParams(param);
+        //tv.addView(textViewNew);
     }
 }
