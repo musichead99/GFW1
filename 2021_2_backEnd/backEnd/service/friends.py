0da950948a1872a2e2bdb30fd8539ae12484521b
ghwns82 <68892482+ghwns82@users.noreply.github.com>
2021-11-21 05:57:24 +0900

    일부 오류 수정, friendsMyRequest 라우터 추가

--- 2021_2_backEnd/backEnd/service/friends.py
+++ 2021_2_backEnd/backEnd/service/friends.py
@@ -20,6 +20,9 @@ FriendsPutRequest = Friends.model('Friend put model', {
 FriendsDeleteRequest = Friends.model('Friend delete model ', {
     "friendEmail" : fields.String(description="친구의 이메일", required=True, example="testemail2@testdomain.com")
 })
+FriendsDeleteRequest2 = Friends.model('Friend delete model 2 ', {
+    "friendEmail" : fields.String(description="친구의 이메일", required=True, example="testemail2@testdomain.com")
+})
 
 FriendsPutFailedRequest = Friends.inherit('Friend put failed model ', swaggerModel.BaseFailedModel, {
     "message" : fields.String(description="에러 메시지", example="already requested or exist")
@@ -35,6 +38,9 @@ FriendsSuccessModel = Friends.inherit('Friends Success model', swaggerModel.Base
 })
 FriendsSuccessModel2 = Friends.inherit('Friends Success model2', swaggerModel.BaseSuccessModel,{
     "FriendsRequestList" : fields.String(description="친구요청목록", example=[{"email": "test8@gmail.com","name": "손흥민","profilePhoto": "http://180.80.221.11:5000/service/image/default_profile.jpg"}])
+})
+FriendsSuccessModel3 = Friends.inherit('Friends Success model3', swaggerModel.BaseSuccessModel,{
+    "MyRequestList" : fields.String(description="나의요청목록", example=[{"email": "test2@gmail.com","name": "김연아","profilePhoto": "http://180.80.221.11:5000/service/image/default_profile.jpg"}])
 })
 FriendsGetFailedRequest = Friends.inherit('Friend get failed model', swaggerModel.BaseFailedModel, {
     "message" : fields.String(description="에러 메시지", example="No friend")
@@ -54,50 +60,51 @@ class AppFriend(Resource):
     @jwt_required()
     @Friends.expect(parser, FriendsPostRequest)
     @Friends.response(200, 'Success', swaggerModel.BaseSuccessModel)
-    @Friends.response(400, 'Failed (전에 이미 친구추가 요청을 보냈다.)', FriendsPutFailed3Request)   
+    @Friends.response(400, 'Failed (전에 이미 친구추가 요청을 보냈다.)', FriendsPutFailedRequest)   
     def post(self, *args):
         """ 사용자로부터 친구 이메일을 인자로 받아서 친구 요청을 보낸다."""
         requester = get_jwt_identity()
         data = request.json
         acceptor = data['friendEmail']
-        print(data)
 
         db = database.DBClass()
         query = f'''
-        SELECT id as id FROM request_friend where requester={requester} and acceptor={acceptor}
+        SELECT id as id FROM request_friend where requester="{requester}" and acceptor="{acceptor}"
         UNION ALL 
-        SELECT id as id FROM friends where user_email={requester} and user_friend_email{acceptor};
+        SELECT id as id FROM friends where user_email="{requester}" and user_friend_email"{acceptor}";
         '''
         if db.executeOne(query):
             return {"status" : "Failed", "message" : "already requested or exist"}, 400
         
         query = f'''
-        insert into request_friend(requester, acceptor) values ({requester}, {acceptor});
+        insert into request_friend(requester, acceptor) values ("{requester}", "{acceptor}");
         '''
         db.execute_and_commit(query)
         return {"status" : "success", "message" : "Your request message has been delivered"}, 200
     
     # 친구요청 수락, 거절
     @validate_params(
-        Param('acceptor', JSON, str, rules=[Pattern(r'^[\w+-_.]+@[\w-]+\.[a-zA-Z-.]+$')]),   # 이메일 형식 체크
+        Param('friendEmail', JSON, str, rules=[Pattern(r'^[\w+-_.]+@[\w-]+\.[a-zA-Z-.]+$')]),   # 이메일 형식 체크
     )
     @jwt_required()
     @Friends.expect(parser, FriendsPutRequest)
     @Friends.doc(params= {"payload":"accept : 수락 여부(대소문자 구별 없음)"})
     @Friends.response(200, 'Success', swaggerModel.BaseSuccessModel)
-    @Friends.response(400, 'Failed ( 친구 입력이 잘못되었다.)', FriendsPutFailedRequest)  
+    @Friends.response(400, 'Failed ( 친구 입력이 잘못되었다.)', FriendsPutFailed3Request)  
     @Friends.response(401, 'Failed (이미 친구이다.)', FriendsPutFailed2Request)  
     def put(self, *args, **kwargs):
         """친구요청을 수락하거나 거절한다."""
-        requester = get_jwt_identity()
+        acceptor = get_jwt_identity()
         data = request.json
-        acceptor = data['acceptor']
+        print(data)
+        requester = data['friendEmail']
+        
         
         db = database.DBClass()
         
         # 입력이 잘못 된 경우
         query = f'''
-        SELECT * FROM request_friend where requester={requester} and acceptor={acceptor}
+        SELECT * FROM request_friend where requester="{requester}" and acceptor="{acceptor}"
         '''
         if not db.executeOne(query):
             return {"status" : "Failed", "message" : "wrong email"}, 400
@@ -105,26 +112,26 @@ class AppFriend(Resource):
         # 친구요청을 거절하는 경우
         if data["accept"].lower() in ["no", "false"]:
             query = f'''
-            delete FROM request_friend where requester={requester} and acceptor={acceptor}
+            delete FROM request_friend where requester="{requester}" and acceptor="{acceptor}"
             '''
             db.execute_and_commit(query, data)
             return {"status" : "success", "message" : "Friend request has been denied"}, 200
 
         # 친구 요청을 수락하는 경우
         query1 = f'''
-            delete FROM request_friend where requester={requester} and acceptor={acceptor}
+            delete FROM request_friend where requester="{requester}" and acceptor="{acceptor}"
             '''
         db.execute_and_commit(query1, data)
         
         query2 = f'''
         SELECT * FROM friends 
-        where user_email={requester} and user_friend_email={acceptor}
-        or user_email={acceptor} and user_friend_email={requester}
+        where user_email="{requester}" and user_friend_email="{acceptor}"
+        or user_email="{acceptor}" and user_friend_email="{requester}"
         ;
         '''
         ## 이미 친구인 경우
         if db.executeAll(query2, data):   
-            return {"status" : "failed", "message" : "aleardy friend"}, 400
+            return {"status" : "failed", "message" : "aleardy friend"}, 401
 
         ## 친구 요청 수락
         query3 ='''
@@ -151,8 +158,8 @@ class AppFriend(Resource):
         # 입력이 잘못 된 경우
         query = f'''
         SELECT * FROM friends 
-        where user_email={user} and user_friend_email={user_friend}
-        or user_email={user_friend} and user_friend_email={user};
+        where user_email="{user}" and user_friend_email="{user_friend}"
+        or user_email="{user_friend}" and user_friend_email="{user}";
         '''
         if not db.executeOne(query):
             return {"status" : "Failed", "message" : "wrong email"}, 400
@@ -162,8 +169,8 @@ class AppFriend(Resource):
         SET SQL_SAFE_UPDATES=0;
         '''
         query2 = f'''
-        delete from friends where user_email={user} and user_friend_email={user_friend}
-        or user_email={user_friend} and user_friend_email={user};
+        delete from friends where user_email="{user}" and user_friend_email="{user_friend}"
+        or user_email="{user_friend}" and user_friend_email="{user}";
         '''
         db.execute_and_commit(query1)
         db.execute_and_commit(query2)
@@ -220,4 +227,59 @@ class AppFriendRequestList(Resource):
 
         return {"status" : "success", "FriendsRequestList" :  result},200
 
+@Friends.route("/friendsMyRequest")
+class AppFriendRequestList(Resource):
+    @jwt_required()
+    @Friends.expect(parser)
+    @Friends.response(200, 'Success(내가 친구요청한 유저목록 반환한다.)', FriendsSuccessModel3)
+    @Friends.response(400, 'Failed ( 요청이 없다. )', FriendsGetFailedRequest2) 
+    def get(self, *args, **kwargs):
+        """사용자가 친구요청한 유저목록을 보여준다"""
+        userEmail = get_jwt_identity()
+
+        db = database.DBClass()
+        query = f'''
+            select email, name, profilePhoto from users, (select acceptor from request_friend where requester = "{userEmail}") as gg where gg.acceptor = email;
+        '''
+        
+        if not(result := db.executeAll(query)):
+            return  {"status" : "Failed", "message" : "No request"}, 400
+
+
+        for i in result:
+
+            if not i['profilePhoto']:
+                i['profilePhoto'] = config.baseUrl + '/service/image/default_profile.jpg'
+
+        return {"status" : "success", "MyRequestList" :  result},200
     
+
+    # 나의 요청 취소
+    @validate_params(
+        Param('friendEmail', JSON, str, rules=[Pattern(r'^[\w+-_.]+@[\w-]+\.[a-zA-Z-.]+$')]),   # 이메일 형식 체크
+    )
+    @jwt_required()
+    @Friends.expect(parser, FriendsDeleteRequest2)
+    @Friends.response(200, 'Success', swaggerModel.BaseSuccessModel)
+    @Friends.response(400, 'Failed ( 친구 입력이 잘못되었다.)', FriendsPutFailed3Request)
+    def delete(self, *args, **kwargs):
+        """친구요청을 취소한다."""
+        requester = get_jwt_identity()
+        data = request.json
+        acceptor = data['friendEmail']
+        
+        db = database.DBClass()
+        
+        # 입력이 잘못 된 경우
+        query = f'''
+        SELECT * FROM request_friend where requester="{requester}" and acceptor="{acceptor}"
+        '''
+        if not db.executeOne(query):
+            return {"status" : "Failed", "message" : "wrong email"}, 400
+
+        # 친구요청 취소
+        query = f'''
+            delete FROM request_friend where requester="{requester}" and acceptor="{acceptor}"
+        '''
+        db.execute_and_commit(query, data)
+        return {"status" : "success", "message" : "Friend request has been canceled"}, 200
